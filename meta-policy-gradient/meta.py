aade2e19f8b2dd091b2260768fb821cd9cd5a48b
Sebastian Lopez-Cot <slcot@mit.edu>
2018-09-28 13:15:48 -0400

    Setting up meta policy framework

---
+++ meta-policy-gradient/meta.py
@@ -0,0 +1,93 @@
+""" 
+Implementation of Meta Policy Gradient
+
+Algorithm and hyperparameter details can be found here: 
+
+    http://arxiv.org/pdf/1509.02971v2.pdf
+
+The algorithm is tested on the Pendulum-v0 OpenAI gym task 
+and developed with tflearn + Tensorflow
+
+Author: Sebastian Lopez-Cot 
+"""
+import tensorflow as tf
+import numpy as np
+import gym
+from gym import wrappers
+import tflearn
+import argparse
+import pprint as pp
+
+# ===========================
+#   Tensorflow Summary Ops
+# ===========================
+
+def build_summaries():
+    episode_reward = tf.Variable(0.)
+    tf.summary.scalar("Reward", episode_reward)
+    episode_ave_max_q = tf.Variable(0.)
+    tf.summary.scalar("Qmax Value", episode_ave_max_q)
+
+    summary_vars = [episode_reward, episode_ave_max_q]
+    summary_ops = tf.summary.merge_all()
+
+    return summary_ops, summary_vars
+
+# ===========================
+#   Agent Training
+# ===========================
+
+def train(sess, env, args, actor, critic, actor_noise):
+
+    # Set up summary Ops
+    summary_ops, summary_vars = build_summaries()
+
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter(args['summary_dir'], sess.graph)
+
+    # TODO add the rest of training
+
+def main(args):
+
+    with tf.Session() as sess:
+
+        env = gym.make(args['env'])
+        np.random.seed(int(args['random_seed']))
+        tf.set_random_seed(int(args['random_seed']))
+        env.seed(int(args['random_seed']))
+
+        state_dim = env.observation_space.shape[0]
+        action_dim = env.action_space.shape[0]
+        action_bound = env.action_space.high
+        # Ensure action bound is symmetric
+        assert (env.action_space.high == -env.action_space.low)
+
+        train(sess, env, args, actor, critic, actor_noise)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='provide arguments for DDPG agent')
+
+    # agent parameters
+    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.0001)
+    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.001)
+    parser.add_argument('--gamma', help='discount factor for critic updates', default=0.99)
+    parser.add_argument('--tau', help='soft target update parameter', default=0.001)
+    parser.add_argument('--buffer-size', help='max size of the replay buffer', default=1000000)
+    parser.add_argument('--minibatch-size', help='size of minibatch for minibatch-SGD', default=64)
+
+    # run parameters
+    parser.add_argument('--env', help='choose the gym env- tested on {Pendulum-v0}', default='Pendulum-v0')
+    parser.add_argument('--random-seed', help='random seed for repeatability', default=1234)
+    parser.add_argument('--max-episodes', help='max num of episodes to do while training', default=50000)
+    parser.add_argument('--max-episode-len', help='max length of 1 episode', default=1000)
+    parser.add_argument('--render-env', help='render the gym env', action='store_true')
+    parser.add_argument('--summary-dir', help='directory for storing tensorboard info', default='./results/tf_meta')
+
+    parser.set_defaults(render_env=False)
+    parser.set_defaults(use_gym_monitor=True)
+    
+    args = vars(parser.parse_args())
+    
+    pp.pprint(args)
+
+    main(args)
---
+++ meta-policy-gradient/train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 meta.py --minibatch-size=64 --buffer-size=1000000 --env=Pendulum-v0 --render-env 
+
